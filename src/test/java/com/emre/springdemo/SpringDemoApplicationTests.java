51b4099373d37e65c3ad6c00369a92e1e2161f1c
Emre Yüce <emre.yuce@simetri8.com>
2025-02-05 18:53:58 +0300

    * initial commit

---
+++ src/test/java/com/emre/springdemo/SpringDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.emre.springdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
